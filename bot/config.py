7ab8e9209fe76ee0c5427c1be252510fa6cbbac7
Nihal Singh <142614932+Emperorns@users.noreply.github.com>
2025-03-17 21:02:24 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,13 +6,13 @@ if Path("config.env").exists():
     load_dotenv("config.env")
 
 class Telegram:
-    API_ID = int(getenv("API_ID", "0"))
-    API_HASH = getenv("API_HASH", "")
+    API_ID = int(getenv("API_ID", "25377875"))
+    API_HASH = getenv("API_HASH", "cf80e342be48570ca2e4c9d2c7695413")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
     PORT = int(getenv("PORT", 8080))
     SESSION_STRING = getenv("SESSION_STRING", "")
     BASE_URL = getenv("BASE_URL", "").rstrip('/')
-    DATABASE_URL = getenv("DATABASE_URL", "")
+    DATABASE_URL = getenv("DATABASE_URL", "mongodb+srv://nehal969797:jinwoo969797@cluster0.5ulvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL", "").split(",") if channel.strip()]
     THEME = getenv("THEME", "quartz").lower()
     USERNAME = getenv("USERNAME", "admin")
